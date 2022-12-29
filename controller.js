e41ee98eb7a4247debf4ea7b24cf4d67f162a811
buginugroho <buginugroho17@gmail.com>
2022-12-29 20:51:02 +0700

    menambahkan fungsi menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku berjalan!", res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaDataMahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
